38725f1da4847f63507fe7663e0f5ec564bf90c9
Ngô Hoàng Thái <37509804+hthai0598@users.noreply.github.com>
2021-03-08 21:49:08 +0700

    Add files via upload

---
+++ RedisDemo/RedisDemo/Configurations/RedisConfigurations.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace RedisDemo.Configurations
+{
+    public class RedisConfigurations
+    {
+        public bool Enable { get; set; }
+        public string ConnectionStrings { get; set; }
+    }
+}
