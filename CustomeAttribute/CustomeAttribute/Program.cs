393ad8b560e1c530a11d2fd782482dbcbfd4369d
Thái Mèo <h.thai0598@gmail.com>
2022-03-07 23:37:17 +0700

    .

---
+++ CustomeAttribute/CustomeAttribute/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+namespace CustomeAttribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var props = typeof(ClientExcel).GetProperties();
+            foreach (var item in props)
+            {
+                Console.WriteLine($"props name {item}");
+                if (!Attribute.IsDefined(item, typeof(ExcelColumnAttribute)))
+                {
+                    Console.WriteLine(item.GetCustomAttribute<ExcelColumnAttribute>().Name);
+                }
+            }
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.All, Inherited = true, AllowMultiple = false)]
+    public class ExcelColumnAttribute : Attribute
+    {
+        public ExcelColumnAttribute(string column)
+        {
+            this.column = column;
+        }
+        public string column { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ClientExcel
+    {
+        [ExcelColumnAttribute("FullName")]
+        public string ClientName { get; set; }
+    }
+}
